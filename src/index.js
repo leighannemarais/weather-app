bf87ea709a117bec4e8572870f3633c1edbe6418
leighannemarais <leighannemarais@gmail.com>
2023-05-16 12:37:55 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+let weather = {
+  paris: {
+    temp: 19.7,
+    humidity: 80,
+  },
+  tokyo: {
+    temp: 17.3,
+    humidity: 50,
+  },
+  lisbon: {
+    temp: 30.2,
+    humidity: 20,
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100,
+  },
+  oslo: {
+    temp: -5,
+    humidity: 20,
+  },
+};
+
+// write your code here
+let city = prompt("Enter a city");
+city = city.toLowerCase().trim();
+
+if (weather[city] !== undefined) {
+  let temp = weather[city].temp;
+  temp = Math.round(temp);
+  let fahrenheitTemp = Math.round((temp * 9) / 5 + 32);
+  let humidity = weather[city].humidity;
+  alert(
+    `The weather in ${city} is ${temp}°C (${fahrenheitTemp}°F) and the humidity is ${humidity}%`
+  );
+} else {
+  alert(
+    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+s${city}`
+  );
+}
